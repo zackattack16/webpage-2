d29071e0ead93bf145d13af0fa4bf5fd0d3d951b
zackattack <zackeryfrederiksen@seattleacademy.org>
2024-01-18 10:30:55 -0800

    input output 2.0000!

--- script.js
+++ script.js
@@ -26,3 +26,30 @@ function changeOut() {
     const x = inBox.value;
     outBox.innerText = x*x
 }
+
+const inBox2 = document.getElementById("in2");
+const outBox2 = document.getElementById("out2");
+
+const selecter = document.getElementById("mathStuff")
+
+inBox2.addEventListener("change", equations);
+
+function equations() {
+    const y = inBox2.value;
+    if (selecter.value == "square") {
+        outBox2.innerText = y*y
+    }
+    else if (selecter.value == "double") {
+        outBox2.innerText = y*2
+
+    }
+    else if (selecter.value == "algrabr") {
+        outBox2.innerText = 2*y + 7
+    }
+    else if (selecter.value == "cube") {
+        outBox2.innerText = y*y*y
+    }
+    else {
+        outBox2.innerText = 0
+    }
+} 
